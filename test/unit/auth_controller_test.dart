8ccb350d250d6c2263fbc21577dbc6913e111972
Rafia Javed <rafia@example.com>
2026-03-15 12:04:46 +0500

    add testing dependencies

--- test/unit/auth_controller_test.dart
+++ test/unit/auth_controller_test.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../lib/features/auth/providers/auth_provider.dart';
+import 'package:firebase_auth_mocks/firebase_auth_mocks.dart';
+
 void main() {
   test('AuthController registers a user', () async {
     final container = ProviderContainer();
