0a080b9aed738cb277a5b113e0f21352e9858c6f
Rafia Javed <rafia@example.com>
2026-03-15 12:50:47 +0500

    configure app routing with authentication screens

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,24 +1,25 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-
-// Import your screens
+import '../../features/auth/screens/login_screen.dart';
 import '../../features/auth/screens/register_screen.dart';
 import '../../features/home/screens/home_screen.dart';
 
-final GoRouter router = GoRouter(
-  initialLocation: '/',
+final router = GoRouter(
   routes: [
+
     GoRoute(
       path: '/',
-      builder: (context, state) => const RegisterScreen(),
+      builder: (context, state) => LoginScreen(),
     ),
+
     GoRoute(
-      path: '/login',
-      builder: (context, state) => const RegisterScreen(), // you can later create LoginScreen
+      path: '/register',
+      builder: (context, state) => RegisterScreen(),
     ),
+
     GoRoute(
       path: '/home',
       builder: (context, state) => const HomeScreen(),
     ),
+
   ],
 );
