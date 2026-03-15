405f4695df985cf653b0178580371a8edef1e153
Rafia Javed <rafia@example.com>
2026-03-16 00:23:33 +0500

    Update golden tests

--- test/unit/auth_controller_test.dart
+++ test/unit/auth_controller_test.dart
@@ -1,8 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test_app/features/auth/providers/auth_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+  TestWidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   test('AuthController registers a user', () async {
     final container = ProviderContainer();
     final authController = container.read(authProvider.notifier);
