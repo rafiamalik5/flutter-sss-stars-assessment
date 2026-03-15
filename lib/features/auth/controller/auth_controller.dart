2dd55ed6fe6ae305dbcd6af385ccef497e90e632
Rafia Javed <rafia@example.com>
2026-03-15 12:37:36 +0500

    Add login screen

---
+++ lib/features/auth/controller/auth_controller.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+final authControllerProvider =
+    StateNotifierProvider<AuthController, User?>((ref) {
+  return AuthController();
+});
+
+class AuthController extends StateNotifier<User?> {
+  AuthController() : super(FirebaseAuth.instance.currentUser);
+
+  final _auth = FirebaseAuth.instance;
+
+  Future<void> register(String email, String password) async {
+    final cred = await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    state = cred.user;
+  }
+
+  Future<void> login(String email, String password) async {
+    final cred = await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    state = cred.user;
+  }
+
+  Future<void> logout() async {
+    await _auth.signOut();
+    state = null;
+  }
+}
